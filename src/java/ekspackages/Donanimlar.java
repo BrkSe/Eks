2b2d53d887edeeb22768110ef564eaa9f9079681
Burak <brkktby@gmail.com>
2015-07-21 11:49:07 +0300

    Donanım Listesi
    Getter ve Setter methodları yapıldı.

---
+++ src/java/ekspackages/Donanimlar.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ekspackages;
+
+/**
+ *
+ * @author Burak KUTBAY
+ * @site www.burakkutbay.com
+ * @blog blog.burakkutbay.com
+ */
+public class Donanimlar {
+
+    private int iddonanim_kayit;
+    private String kullaniciadsoyad;
+    private String bilgisayar_adi;
+    private String ram;
+    private String harddisk;
+    private String ekran_karti;
+    private String os;
+
+    public int getIddonanim_kayit() {
+        return iddonanim_kayit;
+    }
+
+    public String getKullaniciadsoyad() {
+        return kullaniciadsoyad;
+    }
+
+    public String getBilgisayar_adi() {
+        return bilgisayar_adi;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public String getHarddisk() {
+        return harddisk;
+    }
+
+    public String getEkran_karti() {
+        return ekran_karti;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setIddonanim_kayit(int iddonanim_kayit) {
+        this.iddonanim_kayit = iddonanim_kayit;
+    }
+
+    public void setKullaniciadsoyad(String kullaniciadsoyad) {
+        this.kullaniciadsoyad = kullaniciadsoyad;
+    }
+
+    public void setBilgisayar_adi(String bilgisayar_adi) {
+        this.bilgisayar_adi = bilgisayar_adi;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public void setHarddisk(String harddisk) {
+        this.harddisk = harddisk;
+    }
+
+    public void setEkran_karti(String ekran_karti) {
+        this.ekran_karti = ekran_karti;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+}
